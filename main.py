ac298657561b2cb4ac4ef93a6009c89533c69837
morineee <rawlin0501@gmail.com>
2024-05-23 20:59:53 +0900

    start_mobsf()

--- main.py
+++ main.py
@@ -18,25 +18,32 @@ class Main:
         # self.frida_script_path = self.config['Frida'].get('Frida_Script')
         self.encryption_method = self.config['Encryption_method'].get('encryption_method')   
 
-    # def save_config(self):
-    #     with open('config.ini', 'w') as configfile:
-    #         self.config.write(configfile)
-
-    def run_mobsf(self):
-        os_platform = platform.system()
-        print(os_platform)
-        run_script_path = os.path.join(self.mobsf_path, 'setup.sh') # bat / sh
-        if not os.path.exists(run_script_path):
+    def start_mobsf(self):
+        os_name = platform.system()
+        if os_name == "Windows":
+            setup_script_path = os.path.join(self.mobsf_path, 'setup.bat')
+            run_script_path = os.path.join(self.mobsf_path, 'run.bat')
+        elif os_name == "Linux":
+            setup_script_path = os.path.join(self.mobsf_path, 'setup.sh')
+            run_script_path = os.path.join(self.mobsf_path, 'run.sh')
+        elif os_name == "Darwin":
+            setup_script_path = os.path.join(self.mobsf_path, 'setup.sh')
+            run_script_path = os.path.join(self.mobsf_path, 'run.sh')
+        else:
+            print("Unknown operating system.")
+            return
+        if not os.path.exists(setup_script_path):
             print(f"Error: Invalid Path - {self.mobsf_path}")
             print("Please run MobSF Manually")
             return  
-        process = subprocess.Popen(run_script_path, shell=True, cwd=self.mobsf_path)        
-       
-        print("MobSF is starting! you can now enter next commands:")
+        setup_process = subprocess.Popen(setup_script_path, shell=True, cwd=self.mobsf_path)
+        setup_process.communicate()        
+        process = subprocess.Popen(run_script_path, shell=True, cwd=self.mobsf_path)
+        print("MobSF is starting!")
 
         return process 
 
 
 if __name__ == "__main__":
     main = Main()
-    main.run_mobsf()
+    main.start_mobsf()
