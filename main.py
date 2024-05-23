6daf04d044631f13f6087d6b4b42d7d425a58a98
morineee <rawlin0501@gmail.com>
2024-05-23 20:14:29 +0900

    config

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, platform, subprocess
 import configparser
 
 class Main:
@@ -11,18 +11,32 @@ class Main:
         
         self.config.read(config_file_path)
         self.mobsf_path = os.path.join(os.path.dirname(__file__), self.config['MobSF']['MobSF'])
-        self.server_ip = self.config['SERVER'].get('ServerIP', self.config['DEFAULT']['ServerIP'])
-        self.api_key = self.config['API'].get('ApiKey', self.config['DEFAULT']['ApiKey'])
-        self.file_path = self.config['FILE'].get('FilePath', self.config['DEFAULT']['FilePath']).split(',')
-        self.avm_name = self.config['AVM'].get('AVM_Name', self.config['DEFAULT']['AVM_Name'])
-        self.frida_script_path = self.config['Frida'].get('Frida_Script', self.config['DEFAULT']['Frida_Script'])
-        self.encryption_method = self.config['Encryption_method'].get('encryption_method', self.config['DEFAULT']['Encryption_method'])   
+        self.server_ip = self.config['SERVER'].get('ServerIP')
+        self.api_key = self.config['API'].get('ApiKey')
+        self.file_path = self.config['FILE'].get('FilePath').split(',')
+        # self.avm_name = self.config['AVM'].get('AVM_Name')
+        # self.frida_script_path = self.config['Frida'].get('Frida_Script')
+        self.encryption_method = self.config['Encryption_method'].get('encryption_method')   
 
-    def save_config(self):
-        with open('config.ini', 'w') as configfile:
-            self.config.write(configfile)
+    # def save_config(self):
+    #     with open('config.ini', 'w') as configfile:
+    #         self.config.write(configfile)
+
+    def run_mobsf(self):
+        os_platform = platform.system()
+        print(os_platform)
+        run_script_path = os.path.join(self.mobsf_path, 'setup.sh') # bat / sh
+        if not os.path.exists(run_script_path):
+            print(f"Error: Invalid Path - {self.mobsf_path}")
+            print("Please run MobSF Manually")
+            return  
+        process = subprocess.Popen(run_script_path, shell=True, cwd=self.mobsf_path)        
+       
+        print("MobSF is starting! you can now enter next commands:")
+
+        return process 
 
 
 if __name__ == "__main__":
     main = Main()
-    main.start()
+    main.run_mobsf()
