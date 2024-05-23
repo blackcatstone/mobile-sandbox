619bca0a78a98351e3911ccb298990048e63c9ef
morineee <rawlin0501@gmail.com>
2024-05-23 21:45:04 +0900

    is_running()

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os, platform, subprocess
+import os, platform
+import subprocess
 import configparser
+import requests
 
 class Main:
     def __init__(self):
@@ -33,45 +35,55 @@ class Main:
             print("Unknown operating system.")
             return
         if not os.path.exists(setup_script_path):
-            print(f"Error: Invalid Path - {self.mobsf_path}")
-            print("Please run MobSF Manually")
-            return  
+            print(f"Error: Invalid Path: {self.mobsf_path}")
+            return
+        
+        print("Starting MobSF...")
         setup_process = subprocess.Popen(setup_script_path, shell=True, cwd=self.mobsf_path)
         setup_process.communicate()        
         process = subprocess.Popen(run_script_path, shell=True, cwd=self.mobsf_path)
-        print("MobSF is starting!")
-
+        print("MobSF is runnining!")
         return process
 
-    '''def get_status(self): #Print the contents of the config.ini
-        print("=========================HERE=========================")      
-        if hasattr(self, 'server_ip'):
-            print("\nMobSF server IP: {}".format(self.server_ip))
-        else:
-            print("\nMobSF server IP is not set.")
+    # def get_status(self): #Print the contents of the config.ini
+    #     print("=========================HERE=========================")      
+    #     if hasattr(self, 'server_ip'):
+    #         print("\nMobSF server IP: {}".format(self.server_ip))
+    #     else:
+    #         print("\nMobSF server IP is not set.")
 
-        if hasattr(self, 'api_key'):
-            print("MobSF API KEY: {}".format(self.api_key))
-        else:
-            print("MobSF API KEY is not set.")
+    #     if hasattr(self, 'api_key'):
+    #         print("MobSF API KEY: {}".format(self.api_key))
+    #     else:
+    #         print("MobSF API KEY is not set.")
 
-        if hasattr(self, 'file_path'):
-            if self.file_path:
-                print("Target file path ({}): {}".format(len(self.file_path), ', '.join(self.file_path)))
-            else:
-                print("Target file path is not set.\n")
-        else:
-            print("Target file path is not set.\n")
+    #     if hasattr(self, 'file_path'):
+    #         if self.file_path:
+    #             print("Target file path ({}): {}".format(len(self.file_path), ', '.join(self.file_path)))
+    #         else:
+    #             print("Target file path is not set.\n")
+    #     else:
+    #         print("Target file path is not set.\n")
         
-        if hasattr(self, 'mobsf_path'):
-            print("MobSF Path: {}\n".format(self.mobsf_path))
-        else:
-            print("MobSF Path: MobSF Path is not set.")
+    #     if hasattr(self, 'mobsf_path'):
+    #         print("MobSF Path: {}\n".format(self.mobsf_path))
+    #     else:
+    #         print("MobSF Path: MobSF Path is not set.")
 
-        print("=========================HERE=========================")'''
+    #     print("=========================HERE=========================")
 
+    def server_is_running(self):
+        try:
+            response = requests.get(self.server_ip)
+            if response.status_code == 200:
+                return True
+            else:
+                print(f"Warning: The server responded with an unexpected status code: {response.status_code}. Please check the server.")
+                return False
+        except requests.ConnectionError:
+            print("Failed to connect to the server.")
+            return False
 
 if __name__ == "__main__":
     main = Main()
     main.start_mobsf()
-    main.get_status()
